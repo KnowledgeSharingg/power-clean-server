72532c57e21eb1d759106efe7ea838d22210d084
seongkyulim <96x60812@gmail.com>
2024-12-20 22:23:19 +0900

    feat: setting gradle project

---
+++ src/test/kotlin/com/example/power_clean/PowerCleanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.power_clean
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PowerCleanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
