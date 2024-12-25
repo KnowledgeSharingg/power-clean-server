e280d7264cc0692bf40c48d6f6494e511a8f820c
seongkyulim <96x60812@gmail.com>
2024-12-25 17:35:21 +0900

    feat: package setting and securityfilter setting

--- src/main/kotlin/com/example/power_clean/PowerCleanApplication.kt
+++ src/main/kotlin/com/example/power_clean/PowerCleanApplication.kt
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.boot.runApplication
 
 // TODO: securityFilterChain에 설정한 login 옵션 비활성화가 적용되지 않아 임시로 해당 클래스 제외 처리함.
-@SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+// @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
+@SpringBootApplication()
 class PowerCleanApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/example/power_clean/PowerCleanController.kt
+++ src/main/kotlin/com/example/power_clean/PowerCleanController.kt
@@ -1,4 +1,4 @@
-
+package com.example.power_clean
 
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
