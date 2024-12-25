90d0b8a011a24344d51d1c0a813bd9123d711a43
seongkyulim <96x60812@gmail.com>
2024-12-25 17:37:10 +0900

    docs: remove comment in PowerCleanApplication.kt

--- src/main/kotlin/com/example/power_clean/PowerCleanApplication.kt
+++ src/main/kotlin/com/example/power_clean/PowerCleanApplication.kt
@@ -4,8 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-// TODO: securityFilterChain에 설정한 login 옵션 비활성화가 적용되지 않아 임시로 해당 클래스 제외 처리함.
-// @SpringBootApplication(exclude = [SecurityAutoConfiguration::class])
 @SpringBootApplication()
 class PowerCleanApplication
 
