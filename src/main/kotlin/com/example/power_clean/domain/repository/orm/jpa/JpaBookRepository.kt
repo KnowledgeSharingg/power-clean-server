f1ef49a18828b0e80fdd242069b0724700212dc8
seongkyulim <96x60812@gmail.com>
2025-01-05 13:45:52 +0900

    feat: Long -> UUID , add OauthRepository

--- src/main/kotlin/com/example/power_clean/domain/repository/orm/jpa/JpaBookRepository.kt
+++ src/main/kotlin/com/example/power_clean/domain/repository/orm/jpa/JpaBookRepository.kt
@@ -3,6 +3,7 @@ package com.example.power_clean.domain.repository.orm.jpa
 import org.springframework.data.jpa.repository.JpaRepository
 import com.example.power_clean.domain.model.Book
 import com.example.power_clean.domain.repository.BookRepository
+import java.util.UUID
 
-public interface JpaBookRepository : JpaRepository<Book, Long>, BookRepository {
+public interface JpaBookRepository : JpaRepository<Book, UUID>, BookRepository {
 }
--- src/main/kotlin/com/example/power_clean/domain/repository/orm/jpa/JpaPostRepository.kt
+++ src/main/kotlin/com/example/power_clean/domain/repository/orm/jpa/JpaPostRepository.kt
@@ -3,6 +3,7 @@ package com.example.power_clean.domain.repository.orm.jpa
 import org.springframework.data.jpa.repository.JpaRepository
 import com.example.power_clean.domain.model.Post
 import com.example.power_clean.domain.repository.PostRepository
+import java.util.UUID
 
-public interface JpaPostRepository : JpaRepository<Post, Long>, PostRepository {
+public interface JpaPostRepository : JpaRepository<Post, UUID>, PostRepository {
 }
