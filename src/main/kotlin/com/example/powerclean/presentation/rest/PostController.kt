e2f5417d2825a65b1f4e43a8f9d6795c6144cffa
seongkyulim <96x60812@gmail.com>
2025-01-11 16:31:18 +0900

    feat: set swagger in PostController

--- src/main/kotlin/com/example/powerclean/presentation/rest/PostController.kt
+++ src/main/kotlin/com/example/powerclean/presentation/rest/PostController.kt
@@ -6,6 +6,7 @@ import com.example.powerclean.presentation.dto.CreatePostResDto
 import com.example.powerclean.presentation.dto.GetPostDetailResDto
 import com.example.powerclean.presentation.dto.GetPostListResDto
 import com.example.powerclean.presentation.dto.UpdatePostReqDto
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -19,27 +20,32 @@ import java.util.UUID
 @RestController
 @RequestMapping("/post")
 class PostController(private val postService: PostService) {
+    @Operation(summary = "Post 생성 API.", description = "포스트 생성")
     @PostMapping()
     fun createPost(
         @RequestBody request: CreatePostReqDto,
     ): CreatePostResDto = postService.createPost(request)
 
+    @Operation(summary = "Post 상세 조회 API.", description = "포스트 상세 조회.")
     @GetMapping()
     fun getPostDetail(
         @RequestParam postId: UUID,
     ): GetPostDetailResDto = postService.getPostDetail(postId)
 
+    @Operation(summary = "Post 리스트 조회 API.", description = "포스트 리스트 조회.")
     @GetMapping("/list")
     fun getPostList(
         @RequestParam page: Int,
         @RequestParam size: Int,
     ): GetPostListResDto = postService.getPostList(page, size)
 
+    @Operation(summary = "Post 수정 API.", description = "포스트 수정.")
     @PatchMapping()
     fun updatePost(
         @RequestBody request: UpdatePostReqDto,
     ): String = postService.updatePost(request)
 
+    @Operation(summary = "Post 삭제 API.", description = "포스트 삭제.")
     @DeleteMapping()
     fun deletePost(
         @RequestParam postId: UUID,
