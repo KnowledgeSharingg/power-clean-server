9a8e76856c543b45f45da1a2124847141d5cd908
seongkyulim <96x60812@gmail.com>
2025-01-11 16:36:39 +0900

    post 생성 관련 dto에 swagger 적용.

--- src/main/kotlin/com/example/powerclean/presentation/dto/CreateBookReqDto.kt
+++ src/main/kotlin/com/example/powerclean/presentation/dto/CreateBookReqDto.kt
@@ -1,10 +1,15 @@
 package com.example.powerclean.presentation.dto
 
 import com.example.powerclean.domain.valueobject.AuthorInfo
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class CreateBookReqDto(
+    @Schema(description = "책 제목.", example = "이방인")
     val title: String,
+    @Schema(description = "책 내용.", example = "이방인의 삶.")
     val content: String,
+    @Schema(description = "책 정보 링크.", example = "https://search.kyobobook.co.kr/search?keyword=%25EC%259D%25B4%25EB%25B0%25A9%25EC%259D%25B8&gbCode=TOT&target=total")
     val link: String,
+    @Schema(description = "저자 정보.")
     var authorInfo: AuthorInfo,
 )
--- src/main/kotlin/com/example/powerclean/presentation/dto/CreatePostReqDto.kt
+++ src/main/kotlin/com/example/powerclean/presentation/dto/CreatePostReqDto.kt
@@ -1,10 +1,15 @@
 package com.example.powerclean.presentation.dto
 
 import java.util.UUID
+import io.swagger.v3.oas.annotations.media.Schema
 
 data class CreatePostReqDto(
+    @Schema(description = "포스트의 타이틀.", example = "포스트 타이틀!")
     val title: String,
+    @Schema(description = "포스트의 컨텐츠.", example = "포스트 컨텐츠!")
     val content: String,
+    @Schema(description = "포스트 생성자 Account id 값.", example = "123e4567-e89b-12d3-a456-426614174000")
     val creatorAccountId: UUID,
+    @Schema(description = "포스트에 등록될 책 정보.")
     val bookInfo: CreateBookReqDto,
 )
